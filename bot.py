bd6d8d61b3c61c51b1e68c8ecddbf71c4c3b96c0
Alexey <alexeyt101@mail.ru>
2020-11-22 09:49:50 +0300

    Bot first commit

---
+++ bot.py
@@ -0,0 +1,27 @@
+import logging 
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+logging.basicConfig(filename='bot.log', level=logging.INFO)
+
+def main():
+    mybot = Updater('1225759745:AAGIvxdP49ci_1K4i4QjbS2oSyaZ217M-3c', use_context=True)
+
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler('start', green_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
+    logging.info('Бот стартовал')
+    mybot.start_polling()
+    mybot.idle()
+
+def green_user(update, context):
+    print('Вызван /start')
+    update.message.reply_text('Привет, пользователь! Ты вызвал команду /start')
+
+def talk_to_me(update, context):
+    user_text = update.message.text
+    print(user_text)
+    update.message.reply_text(user_text)
+
+
+main()
