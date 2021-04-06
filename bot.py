06cbda00b71874235150eceb11dbacf140f8016a
Alexey Pakhomov <alexeyt101@mail.ru>
2021-04-07 00:09:15 +0300

    Create Updated and dispatcher

--- bot.py
+++ bot.py
@@ -1,27 +1,37 @@
-import logging 
+import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+import settings 
 
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
+PROXY = {
+    'proxy_url': settings.PROXY_URL,
+    'urllib3_proxy_kwargs': {'username': settings.PROXY_USERNAME, 'password': settings.PROXY_PASSWORD},
+}
+
+def greet_user(update, context):
+    print('Вызван /start')
+    update.message.reply_text('Здравствуй, пользователь!')
+
+
+def talk_to_me(update, context):
+    text = update.message.text
+    print(text)
+    update.message.reply_text(text)
+
+
 def main():
-    mybot = Updater('1225759745:AAGIvxdP49ci_1K4i4QjbS2oSyaZ217M-3c', use_context=True)
+    mybot = Updater(settings.API_KEY, use_context=True, request_kwargs=PROXY)
 
     dp = mybot.dispatcher
-    dp.add_handler(CommandHandler('start', green_user))
+    dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
-    logging.info('Бот стартовал')
+
+    logging.info("Бот стартовал")
     mybot.start_polling()
     mybot.idle()
 
-def green_user(update, context):
-    print('Вызван /start')
-    update.message.reply_text('Привет, пользователь! Ты вызвал команду /start')
-
-def talk_to_me(update, context):
-    user_text = update.message.text + ', джан'
-    print(user_text)
-    update.message.reply_text(user_text)
-
-
-main()
+if __name__ == "__main__":
+    main()
